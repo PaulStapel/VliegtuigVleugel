d9c8f37b563317c8925cc97779dc47ecfee8b428
Paul <113509667+PaulStapel@users.noreply.github.com>
2024-03-01 12:41:24 +0100

    aanpassingen aan functie, complex potentiaal geadd.

--- vleugeltje.py
+++ vleugeltje.py
@@ -15,10 +15,18 @@ def gridpoints(R0, Rmax, Nr, Ngamma):
     gamma = np.linspace(0, 2*np.pi, Ngamma) #array for angles
     R = np.linspace(R0, Rmax, Nr) #array for radii
     rr, gg = np.meshgrid(R,gamma) #combine
-    return rr*(np.cos(gg) - 1j*np.sin(gg)) #calculate points and return
+    points = rr*(np.cos(gg) - 1j*np.sin(gg)) 
+    return points.ravel() #flatten your output into a 1D array. 
 
-# TEST. Moet 4 arrays geven (voor de 4 hoeken), met 2 elementen op R = 0 (alles 0) en R = 1.
-#print(gridpoints(0, 1, 2, 4)) 
+# TEST. Moet een array geven met stralen voor alle 4 de hoeken. 
+# print(gridpoints(0, 1, 2, 4))
 
-def complex_potential(U, gamma, mu, z):
-    
+def complex_potential(gamma, mu, z):
+    """
+    Calculate complex potential for U = 1. 
+    gamma = vorticity
+    mu = viscocity
+    z = complex coordinate. 
+    """
+    return z - 1j*gamma*np.log(z)/(2*np.pi) + mu/(2*np.pi*z)
+
