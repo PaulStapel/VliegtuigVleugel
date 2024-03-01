be95e0d377295cffd893af0956b73e2b34548428
Paul <113509667+PaulStapel@users.noreply.github.com>
2024-03-01 12:26:45 +0100

    branch gemaakt voor mijn paul_geklooi

--- vleugeltje.py
+++ vleugeltje.py
@@ -12,12 +12,13 @@ def gridpoints(R0, Rmax, Nr, Ngamma):
 
     Returns nested array, each array is an angle and contains all radii. 
     """
-    gamma = np.linspace(0, 2*np.pi, Ngamma)
-    R = np.linspace(R0, Rmax, Nr)
-    rr, gg = np.meshgrid(R,gamma)
-    return rr*(np.cos(gg) - 1j*np.sin(gg))
+    gamma = np.linspace(0, 2*np.pi, Ngamma) #array for angles
+    R = np.linspace(R0, Rmax, Nr) #array for radii
+    rr, gg = np.meshgrid(R,gamma) #combine
+    return rr*(np.cos(gg) - 1j*np.sin(gg)) #calculate points and return
 
 # TEST. Moet 4 arrays geven (voor de 4 hoeken), met 2 elementen op R = 0 (alles 0) en R = 1.
 #print(gridpoints(0, 1, 2, 4)) 
 
-
+def complex_potential(U, gamma, mu, z):
+    
