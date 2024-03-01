5bc6a4a227cd00f328250d49df56336beb009e15
poffertjess <s.n.kruise@students.uu.nl>
2024-03-01 16:50:00 +0100

    probeersel voor b
    Klopt nog niet, om een of andere reden gaat er iets fout met de complexe waarden. Daarom kloppen er een paar dingen niet qua invoer, maar de output klopt met de theorie. Ik gok dat de foutjes elkaar cancellen

--- probeersel pauls code begrijpen.py
+++ probeersel pauls code begrijpen.py
@@ -1,11 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar  1 13:30:26 2024
-
-@author: sven
-"""
 import numpy as np
 import matplotlib.pyplot as plt
+import Joukowski
 
 def gridpoints(R0, Rmax, Nr, Ngamma):
     """
@@ -20,7 +15,9 @@ def gridpoints(R0, Rmax, Nr, Ngamma):
     gamma = np.linspace(0, 2*np.pi, Ngamma) #array for angles
     R = np.linspace(R0, Rmax, Nr) #array for radii
     rr, gg = np.meshgrid(R,gamma) #combine
-    points = rr*(np.cos(gg) - 1j*np.sin(gg)) 
+    points = rr*(np.cos(gg) + np.sin(gg)*1j) 
+    print(points)
+    print(points.ravel())
     return points.ravel() #flatten your output into a 1D array. 
 
 def complex_potential(gamma, z):
@@ -31,29 +28,26 @@ def complex_potential(gamma, z):
     z = complex coordinate. 
     """
     mu = - 2*np.pi*(np.sqrt(z.real**2 + z.imag**2))
-    return z - 1j*gamma*np.log(z)/(2*np.pi) + mu/(2*np.pi*z)
+    return z - gamma*np.log(z)/(2*np.pi) + mu/(2*np.pi*z)
 
-g = gridpoints(1.12, 2, 500, 500)
-# plt.plot(g.real, g.imag)
+
+g = gridpoints(1.12, 2, 250, 250) 
 
 
 clist = np.zeros(len(g))
 for i in range(len(g)): 
-    clist[i] = complex_potential(0, g[i])
-# plt.scatter(g, clist)
-    
-print(g[0])
+    clist[i] = complex_potential(-3, g[i])
 
-# g.real, g.imag = box_df.x, box_df.y
-  
-# make scalar field
+# print(g[0])
 
-
+a = Joukowski.circle(complex(0,0), 1.12, 100)
+ 
 
 # Plotting scalar field with tricontour
-plt.tricontourf(g.imag, g.real, clist)
-
+plt.tricontourf(g.imag -0.1, g.real +0.22, clist)
+plt.plot(a.real - 0.1, a.imag + 0.22)
 plt.title('Scalar Field')
   
-# Show plot with gird
-plt.grid()
+# # Show plot with gird
+# plt.grid()
+
