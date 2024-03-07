059f0c47eef1e277cbdcb5eba3fc858a98cc879c
poffertjess <s.n.kruise@students.uu.nl>
2024-03-07 18:43:39 +0100

    Update zoomplotje.py

--- zoomplotje.py
+++ zoomplotje.py
@@ -5,10 +5,13 @@ def zoomplotje(joukowski_wing, x,y,z):
     xmax_wing,xmin_wing = max(joukowski_wing.real), min(joukowski_wing.real)
     ymax_wing,ymin_wing = max(joukowski_wing.imag), min(joukowski_wing.imag)
     
+    xl,yl = (xmax_wing - xmin_wing), (ymax_wing - ymin_wing)
+
+    
     ypoint = joukowski_wing.imag[joukowski_wing.real.index(xmax_wing)]
     
-    xmax,xmin = xmax_wing + 0.1*(xmax_wing - xmin_wing), xmax_wing - 0.1*(xmax_wing - xmin_wing)
-    ymax,ymin = ypoint + 0.1*(ymax_wing - ymin_wing),  ypoint - 0.1*(ymax_wing - ymin_wing)
+    xmax,xmin = xmax_wing + 0.1*xl, xmax_wing - 0.1*xl
+    ymax,ymin = ypoint + 0.1*yl,  ypoint - 0.1*yl
     
     plt.plot()
     plt.tricontour(x,y,z)
